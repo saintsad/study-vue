428447c915a6674df117977abf3e01668dfca97c
saintsad <saintsad@gmail.com>
2021-01-28 08:16:46 +0900

    ADD A2 2

---
+++ 00/src/components/A2.vue
@@ -0,0 +1,18 @@
+<template>
+    <a :href="url">
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+    export default {
+        name: 'A2',
+        props : {
+            url: String
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
