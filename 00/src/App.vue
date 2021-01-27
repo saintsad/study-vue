371ebffbb0e66a693f947fe364307b8dc43abb07
saintsad <saintsad@gmail.com>
2021-01-28 07:52:38 +0900

    change title

--- 00/src/App.vue
+++ 00/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="어떻게 이렇게 바뀌지?"/>
   </div>
 </template>
 
