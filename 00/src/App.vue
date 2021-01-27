2dfc88a5dbebdb45eba5111596797808649cc6b8
saintsad <saintsad@gmail.com>
2021-01-28 08:16:15 +0900

    ADD A2

--- 00/src/App.vue
+++ 00/src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="어떻게 이렇게 바뀌지?"/>
+    <A2 url="http://www.naver.com">네이버</A2>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import A2 from './components/A2.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    A2
   }
 }
 </script>
