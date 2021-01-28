34d8068b78b7559a96cd0b8c249e41026605733e
saintsad <saintsad@gmail.com>
2021-01-28 11:40:33 +0900

    add Modal

--- 00/src/App.vue
+++ 00/src/App.vue
@@ -3,17 +3,32 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="어떻게 이렇게 바뀌지?"/>
     <A2 url="http://www.naver.com">네이버</A2>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <Modal v-if="showModal" @close="showModal = false">
+      <div slot="body">dsfdsfsdf<br/> dsfdsfdsf<br/> dsfdsfsdfsdfdsfdsfdsf</div>
+      <h1 slot="header">{{title}}</h1>
+    </Modal>
+    <input type="text" v-model="title" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import A2 from './components/A2.vue'
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    A2
+    A2,
+    Modal
+  },
+  data(){
+    return {
+      title: '',
+      showModal : false
+    }
   }
 }
 </script>
